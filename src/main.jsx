05a7e66e54d91a62388d15e230dcab539fcec8ae
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-04-18 22:52:22 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/SWIFTTECH-WEBSITE/">
       <App />
     </Router>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/swifttech-website/',
+  base: '/SWIFTTECH-WEBSITE/',
   plugins: [react(),
     tailwindcss(),
   ],
