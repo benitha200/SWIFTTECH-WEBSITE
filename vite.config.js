e0f9b517b37b4935ed897eb3cf01ba1eccce09d7
benitha200 <benithaiyuyisenga2002@gmail.com>
2025-04-18 22:48:45 +0200

    chagse

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SWIFTTECH-WEBSITE/',
+  base: '/swifttech-website/',
   plugins: [react(),
     tailwindcss(),
   ],
